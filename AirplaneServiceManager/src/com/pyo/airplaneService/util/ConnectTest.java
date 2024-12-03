57eb14130fb58e76ab476f8977fe1b5eb28ed7fd
jangpyo <wkdvy122851@gmail.com>
2024-12-03 09:05:04 +0900

    [CREATE] 로컬-AIRPLANEBOOKING DB 연동 테스트

---
+++ AirplaneServiceManager/src/com/pyo/airplaneService/util/ConnectTest.java
@@ -0,0 +1,15 @@
+package com.pyo.airplaneService.util;
+
+import java.sql.Connection;
+
+public class ConnectTest {
+	//데이터베이스 DB 연결 접속 테스트
+	public static void main(String[] args) {
+		Connection con = DBUtility.dbCon();
+		if (con != null) {
+			System.out.println("데이타베이스 접속 성공");
+		} else {
+			System.out.println("데이타베이스 접속 실패");
+		}
+	}
+}
